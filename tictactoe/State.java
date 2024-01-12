2b3fec751df89372370e16778f8611b69c40b2d6
Simon Pak <simon.pak@mrjaffesclass.com>
2024-01-12 11:43:20 -0800

    Stuck on tictactoe

---
+++ tictactoe/State.java
@@ -0,0 +1,62 @@
+package tictactoe;
+
+/**
+ * Tic-Tac-Toe state variables.
+ */
+public class State
+{
+    private int gameState = Constants.STANDBY;
+    private int whoseMove = Constants.X;
+    private String xName = "";
+    private String oName = "";
+    private int[][] board = new int[Constants.BOARD_SIZE][Constants.BOARD_SIZE];
+
+    public boolean isWinner() {
+        // You will write this code!!
+    }
+
+    public boolean isTie() {
+        // You will write this code too!!
+    }
+
+    public int getGameState() {
+        return this.gameState;
+    }
+
+    public void setGameState(int gameState) {
+        this.gameState = gameState;
+    }
+    
+    public int getWhoseMove(){
+        return this.whoseMove;
+    }
+    
+    public void setWhoseMove(int whoseMove){
+        this.whoseMove = whoseMove;
+    }
+    
+    public String setXName(){
+        return this.xName;
+    }
+    
+    public void getXName(String xName){
+        this.xName = xName;
+    }
+    
+    public String setOName(){
+        return this.oName;
+    }
+    
+    public void getOName(String oName){
+        this.oName = oName;
+    }
+    
+        public int getBoardCell(int row, int col) {
+            return this.board[row][col];
+    }
+
+    public void setBoardCell(int row, int col, int value) {
+        this.board[row][col] = value;
+    }
+
+}
