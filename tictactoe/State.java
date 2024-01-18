f87046c206b747047ed01cb69002c73df73cd2ce
Simon Pak <simon.pak@mrjaffesclass.com>
2024-01-18 10:38:12 -0800

    On debugging

--- tictactoe/State.java
+++ tictactoe/State.java
@@ -12,47 +12,67 @@ public class State
     private int[][] board = new int[Constants.BOARD_SIZE][Constants.BOARD_SIZE];
 
     public boolean isWinner() {
-        // You will write this code!!
+        int total;
+        for (int row=0; row<Constants.BOARD_SIZE; row++) {
+            total = getBoardCell(row, 0) + getBoardCell(row,1) + getBoardCell(row,2);
+            if (total == -3 || total == 3) return true;
+        }
+        for (int col=0; col<Constants.BOARD_SIZE; col++) {
+            total = getBoardCell(0, col) + getBoardCell(1,col) + getBoardCell(2, col);
+            if (total == -3 || total == 3) return true;
+        }
+        total = getBoardCell(0, 0) + getBoardCell(1,1) + getBoardCell(2, 2);
+        if (total == -3 || total == 3) return true;
+        total = getBoardCell(2, 0) + getBoardCell(1,1) + getBoardCell(0, 2);
+        if (total == -3 || total == 3) return true;
+        return false;
     }
 
     public boolean isTie() {
-        // You will write this code too!!
+        for (int row=0; row<Constants.BOARD_SIZE; row++) {
+            for (int col=0; col<Constants.BOARD_SIZE; col++) {
+                if (getBoardCell(row,col) == Constants.BLANK) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     public int getGameState() {
-        return this.gameState;
+        return gameState;
     }
 
     public void setGameState(int gameState) {
         this.gameState = gameState;
     }
-    
-    public int getWhoseMove(){
-        return this.whoseMove;
+
+    public int getWhoseMove() {
+        return whoseMove;
     }
-    
-    public void setWhoseMove(int whoseMove){
+
+    public void setWhoseMore(int whoseMove) {
         this.whoseMove = whoseMove;
     }
-    
-    public String setXName(){
-        return this.xName;
+
+    public String getXName() {
+        return xName;
     }
-    
-    public void getXName(String xName){
+
+    public void setXName(String xName) {
         this.xName = xName;
     }
-    
-    public String setOName(){
-        return this.oName;
+
+    public String getOName() {
+        return oName;
     }
-    
-    public void getOName(String oName){
+
+    public void setOName(String oName) {
         this.oName = oName;
     }
-    
-        public int getBoardCell(int row, int col) {
-            return this.board[row][col];
+
+    public int getBoardCell(int row, int col) {
+        return this.board[row][col];
     }
 
     public void setBoardCell(int row, int col, int value) {
@@ -60,3 +80,4 @@ public class State
     }
 
 }
+
